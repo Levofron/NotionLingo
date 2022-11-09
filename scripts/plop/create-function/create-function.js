82f1fcc347e40b65e5c6517ee7329a9efbe570f9
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-11-09 17:24:04 +0100

    chore: configure plop (#20)

---
+++ scripts/plop/create-function/create-function.js
@@ -0,0 +1,35 @@
+const path = require('node:path');
+const inquirerDirectory = require('inquirer-directory');
+
+module.exports = (plop) => {
+  plop.addPrompt('directory', inquirerDirectory);
+  plop.addHelper('absPath', (_absPath) =>
+    path.resolve(plop.getPlopfilePath(), '../../../src', _absPath),
+  );
+
+  plop.setGenerator('function', {
+    prompts: [
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Please enter a function name (lowercase, with spaces):',
+      },
+      {
+        name: 'path',
+        type: 'directory',
+        basePath: './src',
+        message: 'Where would you like to put this function?',
+      },
+    ],
+    actions: [
+      {
+        verbose: true,
+        type: 'addMany',
+        base: 'templates',
+        stripExtensions: ['hbs'],
+        templateFiles: 'templates/*.hbs',
+        destination: '{{absPath path}}/{{kebabCase name}}',
+      },
+    ],
+  });
+};
