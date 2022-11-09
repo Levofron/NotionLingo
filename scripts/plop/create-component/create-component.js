f60de71b9d93270c2495dc2742f4e3fd2cfa36dc
playerony <pawel.wojtasinski.1995@gmail.com>
2022-11-09 17:19:53 +0100

    chore: configure plop

---
+++ scripts/plop/create-component/create-component.js
@@ -0,0 +1,45 @@
+const path = require('node:path');
+const inquirerDirectory = require('inquirer-directory');
+
+module.exports = (plop) => {
+  plop.addPrompt('directory', inquirerDirectory);
+  plop.addHelper('absPath', (_absPath) =>
+    path.resolve(plop.getPlopfilePath(), '../../../src', _absPath),
+  );
+  plop.addPartial(
+    'componentNameInBracesPartial',
+    (name, str) => `{${str.helpers.properCase(name.name, 0)}}`,
+  );
+
+  plop.setGenerator('component', {
+    prompts: [
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Please enter a component name (lowercase, with spaces):',
+      },
+      {
+        type: 'list',
+        name: 'type',
+        choices: ['component', 'layout', 'template', 'page'],
+        message: 'What type of component do you want to generate?',
+      },
+      {
+        name: 'path',
+        type: 'directory',
+        basePath: './src',
+        message: 'Where would you like to put this component?',
+      },
+    ],
+    actions: [
+      {
+        verbose: true,
+        type: 'addMany',
+        base: 'templates',
+        stripExtensions: ['hbs'],
+        templateFiles: 'templates/*.hbs',
+        destination: '{{absPath path}}/{{kebabCase name}}',
+      },
+    ],
+  });
+};
---
+++ scripts/plop/create-hook/create-hook.js
@@ -0,0 +1,41 @@
+const path = require('node:path');
+const inquirerDirectory = require('inquirer-directory');
+
+module.exports = (plop) => {
+  plop.addPrompt('directory', inquirerDirectory);
+  plop.addHelper('absPath', (_absPath) =>
+    path.resolve(plop.getPlopfilePath(), '../../../src', _absPath),
+  );
+
+  plop.setGenerator('hook', {
+    prompts: [
+      {
+        name: 'name',
+        type: 'input',
+        message: 'Please enter a hook name (lowercase, with spaces):',
+      },
+      {
+        type: 'list',
+        name: 'extension',
+        choices: ['ts', 'tsx'],
+        message: 'What extension will your hook have?',
+      },
+      {
+        name: 'path',
+        type: 'directory',
+        basePath: './src',
+        message: 'Where would you like to put this hook?',
+      },
+    ],
+    actions: [
+      {
+        verbose: true,
+        type: 'addMany',
+        stripExtensions: ['hbs'],
+        base: 'templates',
+        templateFiles: 'templates/*.hbs',
+        destination: '{{absPath path}}/{{kebabCase name}}',
+      },
+    ],
+  });
+};
