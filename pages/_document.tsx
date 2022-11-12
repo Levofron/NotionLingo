16761b6b74f469b4f09275a8aa47535db8b35b6f
playerony <pawel.wojtasinski.1995@gmail.com>
2022-11-12 11:42:45 +0100

    chore: improve pwa with new images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="/manifest.json" rel="manifest" />
-          <link href="/icon-192x192.png" rel="apple-touch-icon" />
-          <meta content="#042940" name="theme-color" />
+          <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+          <meta content="#F5F5F5" name="theme-color" />
+          <link href="/favicon.ico" rel="icon" />
+          <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+          <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
         </Head>
         <body>
           <Main />
