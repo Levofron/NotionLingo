7e78b15192649fa0be5a2e6a0d0f588ee500a761
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-11-13 14:56:01 +0100

    feat: create supabase instance (#28)

---
+++ src/infrastructure/supabase/supabase.instance.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+export const supabaseInstance = createClient(supabaseUrl, supabaseKey);
