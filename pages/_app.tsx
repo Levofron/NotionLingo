763f1a084b9c1c197fe02274aca881fb108c488b
playerony <pawel.wojtasinski.1995@gmail.com>
2022-11-16 17:28:40 +0100

    fix: import location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
+import { useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 import { createDevToolsClient } from '@ui/utils';
 
 import '../styles/globals.css';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
