8107213bb7d140ecdcc516a2655fa5f229e7ea7d
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-11-17 17:47:35 +0100

    feat: create random phrases todo endpoint (#35)

--- pages/api/notion/random-phrases.ts
+++ pages/api/notion/random-phrases.ts
@@ -1,14 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'PUT') {
-    res.status(405).send({ message: 'Only PUT requests allowed' });
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' });
 
     return;
   }
 
   // TODO - validate header key
   // TODO - add login validation
+  // TODO - validate body params
   // TODO - create update profile endpoint
   // TODO - allow user to change only api_key and page_id
 
