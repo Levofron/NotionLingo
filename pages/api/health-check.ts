f1cad5538a36d874fa84a5d571335b6565197217
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-11-20 11:46:46 +0100

    feat: create simple middleware system for api (#40)

--- pages/api/health-check.ts
+++ pages/api/health-check.ts
@@ -1,16 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'GET') {
-    res.status(405).send({ message: 'Only GET requests allowed' });
+import { withMiddleware, validateRequestMethodMiddleware } from './utils';
 
-    return;
-  }
-
+const handler = (_: NextApiRequest, res: NextApiResponse) => {
   const currentDate = new Date();
   const isoDate = currentDate.toISOString();
 
   res.status(200).send(isoDate);
 };
 
-export default handler;
+const middlewareToApply = [validateRequestMethodMiddleware('GET')];
+
+export default withMiddleware(handler)(middlewareToApply);
