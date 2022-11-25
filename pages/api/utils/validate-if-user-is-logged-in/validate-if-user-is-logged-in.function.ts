27e72669e40c679f268e4305f465b411ee19ef1a
playerony <pawel.wojtasinski.1995@gmail.com>
2022-11-25 13:54:54 +0100

    feat: create validate if user is logged in middleware

---
+++ pages/api/utils/validate-if-user-is-logged-in/validate-if-user-is-logged-in.function.ts
@@ -0,0 +1,14 @@
+import { supabaseInstance } from '@infrastructure';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const validateIfUserIsLoggedIn = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { user } = await supabaseInstance.auth.api.getUserByCookie(req);
+
+  if (!user) {
+    res.status(401).send({ message: 'Unauthorized user' });
+
+    return false;
+  }
+
+  return true;
+};
