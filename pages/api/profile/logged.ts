d98443b6c468d7f17bb2b2788e734ed4223739fe
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-11-30 10:35:30 +0100

    feat: create get logged user endpoint (#51)

--- pages/api/profile/logged.ts
+++ pages/api/profile/logged.ts
@@ -4,15 +4,35 @@ import {
   withMiddleware,
   validateRequestMethodMiddleware,
   validateRouteSecretMiddleware,
+  validateIfUserIsLoggedIn,
+  assignRequestTokenToSupabaseSessionMiddleware,
 } from '../utils';
 
-const handler = (_: NextApiRequest, res: NextApiResponse) => {
-  // TODO - add login validation
-  // TODO - create get logged profile endpoint
+import { supabaseInstance } from '@infrastructure';
 
-  res.status(200).json({ name: 'John Doe' });
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { user } = await supabaseInstance.auth.api.getUserByCookie(req);
+
+  const { data: profilesData, error: profilesError } = await supabaseInstance
+    .from('profiles')
+    .select('email,created_at,updated_at')
+    .eq('id', user?.id)
+    .single();
+
+  if (profilesError) {
+    return res.status(500).send(profilesError);
+  }
+
+  const mergedUser = { ...user, ...profilesData };
+
+  res.status(200).json(mergedUser);
 };
 
-const middlewareToApply = [validateRequestMethodMiddleware('GET'), validateRouteSecretMiddleware];
+const middlewareToApply = [
+  validateRequestMethodMiddleware('GET'),
+  validateRouteSecretMiddleware,
+  validateIfUserIsLoggedIn,
+  assignRequestTokenToSupabaseSessionMiddleware,
+];
 
 export default withMiddleware(handler)(middlewareToApply);
