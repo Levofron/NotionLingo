af2057b91baa10198063e09b6f7164711689993f
playerony <pawel.wojtasinski.1995@gmail.com>
2022-12-02 22:47:36 +0100

    feat: create get available notion pages endpoint

--- pages/api/notion/available-pages.ts
+++ pages/api/notion/available-pages.ts
@@ -7,12 +7,50 @@ import {
   validateRouteSecretMiddleware,
   validateIfUserIsLoggedIn,
   assignRequestTokenToSupabaseSessionMiddleware,
+  decrypt,
+  createNotionClient,
 } from '../utils';
+
+import { supabaseInstance } from '@infrastructure';
+import { DatabaseObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const user = await getUserFromRequest(req);
 
-  res.status(200).json(user);
+  const { data: profilesData, error: profilesError } = await supabaseInstance
+    .from('profiles')
+    .select('notion_api_key')
+    .eq('id', user?.id)
+    .single();
+
+  if (profilesError) {
+    return res.status(500).send(profilesError);
+  }
+
+  try {
+    const hash = JSON.parse(profilesData.notion_api_key);
+    const notionApiKey = decrypt(hash);
+
+    const notionClient = createNotionClient(notionApiKey);
+
+    const availablePages = await notionClient.search({
+      filter: { value: 'database', property: 'object' },
+    });
+
+    const parsedAvailablePages = (availablePages.results as DatabaseObjectResponse[]).map(
+      (_page) => ({
+        id: _page.id,
+        url: _page.url,
+        createdTime: _page.created_time,
+        title: _page.title[0].plain_text,
+        lastEditedTime: _page.last_edited_time,
+      }),
+    );
+
+    return res.status(200).json(parsedAvailablePages);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 };
 
 const middlewareToApply = [
