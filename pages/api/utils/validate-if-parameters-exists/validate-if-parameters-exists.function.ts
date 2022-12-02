073a3189aa95f9ef1ea98cf316e6aca2625cbcb0
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-12-02 16:19:58 +0100

    feat: create validate if parameters exists middleware (#54)

---
+++ pages/api/utils/validate-if-parameters-exists/validate-if-parameters-exists.function.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const validateIfParametersExists =
+  (parametersDestination: 'body' | 'query', requiredParameters: string[]) =>
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const requestParameters = req[parametersDestination];
+    const requestParametersKeys = Object.keys(requestParameters);
+
+    const hasParameters = requestParametersKeys?.every((_requestParametersKey) =>
+      requiredParameters.includes(requestParameters[_requestParametersKey]),
+    );
+
+    if (!hasParameters) {
+      res.status(400).send({
+        message: `Incorrect request. Your "${parametersDestination}" does not contain all required arguments`,
+      });
+
+      return false;
+    }
+
+    return true;
+  };
