fb038873c9b48f91e90a4762506e4399d04e075f
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-12-06 13:01:36 +0100

    chore: disable pwa for local (#65)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  // disable: process.env.NEXT_PUBLIC_APP_ENV === 'local',
+  disable: process.env.NEXT_PUBLIC_APP_ENV === 'local',
 });
 
 module.exports = withPWA({
