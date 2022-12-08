6f27bf72d9d4d6390d7304375d4ce421c6bac08a
playerony <pawel.wojtasinski.1995@gmail.com>
2022-12-08 19:58:54 +0100

    chore: add tests for create contion client function

---
+++ pages/api/utils/create-notion-client/create-notion-client.spec.ts
@@ -0,0 +1,16 @@
+import { functionImportTest } from '@utils';
+
+import { createNotionClient } from './create-notion-client.function';
+
+describe('createNotionClient function', () => {
+  functionImportTest(createNotionClient);
+
+  it('should return a Notion Client', () => {
+    const token = 'token';
+    const client = createNotionClient(token);
+
+    const clientkeys = Object.keys(client);
+
+    expect(clientkeys).toEqual(['blocks', 'databases', 'pages', 'users', 'comments', 'search']);
+  });
+});
