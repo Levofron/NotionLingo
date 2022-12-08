221ba2645c022c157381ac1d08d54d05b298afbe
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-12-08 19:43:39 +0100

    chore: add tests for shuffle array function (#71)

---
+++ pages/api/utils/shuffle-array/shuffle-array.spec.ts
@@ -0,0 +1,29 @@
+import { functionImportTest } from '@utils';
+
+import { shuffleArray } from './shuffle-array.function';
+
+describe('shuffleArray function', () => {
+  functionImportTest(shuffleArray);
+
+  describe('should return passed array scenario', () => {
+    it('has only one element', () => {
+      const array = [1];
+
+      expect(shuffleArray(array)).toEqual(array);
+    });
+
+    it('has zero elements', () => {
+      const array: number[] = [];
+
+      expect(shuffleArray(array)).toEqual(array);
+    });
+  });
+
+  describe('should return shuffled array scenario', () => {
+    it('is going to be shuffled', () => {
+      const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+      expect(shuffleArray(array)).not.toEqual(array);
+    });
+  });
+});
