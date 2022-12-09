a77a5eac2bceb9551f1987d65fa99c8057edca0e
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-12-09 13:24:27 +0100

    chore: add simple tests for get user from reuest (#73)

--- pages/api/utils/get-user-from-request/get-user-from-request.spec.ts
+++ pages/api/utils/get-user-from-request/get-user-from-request.spec.ts
@@ -4,4 +4,11 @@ import { getUserFromRequest } from './get-user-from-request.function';
 
 describe('getUserFromRequest function', () => {
   functionImportTest(getUserFromRequest);
+
+  it('should return null if no user is found', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const user = await getUserFromRequest({} as any);
+
+    expect(user).toBeNull();
+  });
 });
