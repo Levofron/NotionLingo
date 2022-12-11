edc224cd2f23478d63fe653825930932c60c45ea
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-12-11 14:58:51 +0100

    test: validate request method middleware (#80)

---
+++ pages/api/utils/validate-request-method-middleware/validate-request-method-middleware.spec.ts
@@ -0,0 +1,35 @@
+import { functionImportTest } from '@utils';
+
+import { validateRequestMethodMiddleware } from './validate-request-method-middleware.function';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const response: any = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn().mockReturnThis(),
+};
+
+describe('validateRequestMethodMiddleware function', () => {
+  functionImportTest(validateRequestMethodMiddleware);
+
+  it('should return false if request method is not valid', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const request: any = {
+      method: 'POST',
+    };
+
+    const result = await validateRequestMethodMiddleware('GET')(request, response);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true if request method is valid', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const request: any = {
+      method: 'GET',
+    };
+
+    const result = await validateRequestMethodMiddleware('GET')(request, response);
+
+    expect(result).toBeTruthy();
+  });
+});
