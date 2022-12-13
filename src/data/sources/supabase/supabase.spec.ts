825cc66fc591a829592e1f9d0a0a90bc6793234c
playerony <pawel.wojtasinski.1995@gmail.com>
2022-12-13 18:51:05 +0100

    test: supabase source

---
+++ src/data/sources/supabase/supabase.spec.ts
@@ -0,0 +1,91 @@
+import { functionImportTest } from '@utils';
+
+import { getSupabaseSource } from './supabase.source';
+
+describe('supabase source', () => {
+  functionImportTest(getSupabaseSource);
+
+  it('should return proper object', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const supabaseInstanceMock: any = {};
+
+    const supabaseSource = getSupabaseSource(supabaseInstanceMock);
+
+    expect(supabaseSource).toEqual({
+      signIn: expect.any(Function),
+      logout: expect.any(Function),
+      getUser: expect.any(Function),
+      getSession: expect.any(Function),
+    });
+  });
+
+  it('should call signIn with proper params', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const supabaseInstanceMock: any = {
+      auth: {
+        signIn: jest.fn(),
+      },
+    };
+
+    const supabaseSource = getSupabaseSource(supabaseInstanceMock);
+
+    supabaseSource.signIn('google');
+
+    expect(supabaseInstanceMock.auth.signIn).toHaveBeenCalledWith({
+      provider: 'google',
+    });
+  });
+
+  it('should call logout', () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const supabaseInstanceMock: any = {
+      auth: {
+        signOut: jest.fn(),
+      },
+    };
+
+    const supabaseSource = getSupabaseSource(supabaseInstanceMock);
+
+    supabaseSource.logout();
+
+    expect(supabaseInstanceMock.auth.signOut).toHaveBeenCalled();
+  });
+
+  it('should call getUser', async () => {
+    const userMock = {};
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const supabaseInstanceMock: any = {
+      auth: {
+        user: jest.fn().mockReturnValue(userMock),
+      },
+    };
+
+    const supabaseSource = getSupabaseSource(supabaseInstanceMock);
+
+    supabaseInstanceMock.auth.user.mockReturnValue(userMock);
+
+    const user = await supabaseSource.getUser();
+
+    expect(user).toEqual(userMock);
+  });
+
+  it('should call getSession', async () => {
+    const sessionMock = {};
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const supabaseInstanceMock: any = {
+      auth: {
+        session: jest.fn().mockReturnValue(sessionMock),
+      },
+    };
+
+    const supabaseSource = getSupabaseSource(supabaseInstanceMock);
+
+    supabaseInstanceMock.auth.session.mockReturnValue(sessionMock);
+
+    const session = await supabaseSource.getSession();
+
+    expect(session).toEqual(sessionMock);
+  });
+});
