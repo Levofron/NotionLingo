81b75d497a5991d8baf11af5b755dd7281de7b0f
playerony <pawel.wojtasinski.1995@gmail.com>
2022-12-26 18:55:54 +0100

    chore: update

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,13 @@
-export const SUPPORTED_WORD_COLUMN_NAMES = ['Word'];
-export const SUPPORTED_MEANING_COLUMN_NAMES = ['Meaning'];
-export const SUPPORTED_EXAMPLE_SENTENCE_COLUMN_NAMES = ['Example sentence'];
+export const SUPPORTED_EXAMPLE_SENTENCE_COLUMN_NAMES = [
+  'example',
+  'Example',
+  'Sentence',
+  'sentence',
+  'Example sentence',
+  'example sentence',
+];
+export const SUPPORTED_MEANING_COLUMN_NAMES = ['Meaning', 'meaning'];
+export const SUPPORTED_WORD_COLUMN_NAMES = ['Word', 'word', 'Name', 'name'];
 
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const API_ROUTE_SECRET = process.env.NEXT_PUBLIC_API_ROUTE_SECRET;
