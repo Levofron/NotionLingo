46c3ec8ac833a413018e6f459b1d1eac2526351c
playerony <pawel.wojtasinski.1995@gmail.com>
2022-12-26 10:11:59 +0100

    chore: create more flexible way of getting columns data

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const SUPPORTED_WORD_COLUMN_NAMES = ['word'];
-export const SUPPORTED_MEANING_COLUMN_NAMES = ['meaning'];
-export const SUPPORTED_EXAMPLE_SENTENCE_COLUMN_NAMES = ['example sentence'];
+export const SUPPORTED_WORD_COLUMN_NAMES = ['Word'];
+export const SUPPORTED_MEANING_COLUMN_NAMES = ['Meaning'];
+export const SUPPORTED_EXAMPLE_SENTENCE_COLUMN_NAMES = ['Example sentence'];
 
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const API_ROUTE_SECRET = process.env.NEXT_PUBLIC_API_ROUTE_SECRET;
--- src/infrastructure/utils/node/is-valid-notion-page-schema/is-valid-notion-page-schema.types.ts
+++ src/infrastructure/utils/node/is-valid-notion-page-schema/is-valid-notion-page-schema.types.ts
@@ -2,3 +2,8 @@ export interface IPageProperties {
   name: string;
   type: string;
 }
+
+export interface IValidPageProperties {
+  names: string[];
+  type: string;
+}
