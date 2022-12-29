d36a827242dfde38e3edd3872bda40ea886bca2f
playerony <pawel.wojtasinski.1995@gmail.com>
2022-12-29 15:54:30 +0100

    ci: create command to generate enum from pages directory structure

---
+++ scripts/node/convert-pages-directory-to-routes-enum.js
@@ -0,0 +1,77 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const getPath = (destination) => {
+  const currentDirectory = process.cwd();
+
+  return path.join(currentDirectory, destination);
+};
+
+const getPagesDirectoryPath = () => getPath('pages');
+
+const getRouteTypesDirectoryPath = () => getPath('src/infrastructure/types/routes.ts');
+
+const getFilenamesFromDirectory = (dirname) =>
+  new Promise((resolve, reject) => {
+    fs.readdir(dirname, (error, filenames) => {
+      if (error) {
+        reject(error);
+
+        return;
+      }
+
+      const filteredFilenames = filenames.filter((filename) =>
+        fs.lstatSync(`${dirname}/${filename}`).isFile(),
+      );
+
+      resolve(filteredFilenames);
+    });
+  });
+
+const filterOutFilesWithFloorAtBeginning = (filenames) =>
+  filenames.filter((filename) => !filename.startsWith('_'));
+
+const removeExtensionsFromFilenames = (filenames) =>
+  filenames.map((filename) => filename.replace(/\.[^./]+$/, ''));
+
+const replace = (string, search, replaceWith) => string.split(search).join(replaceWith);
+
+const convertNamesToEnumBody = (names) =>
+  names
+    .map((name) => {
+      const variableName = replace(name, '-', '_');
+
+      if (variableName === 'index') {
+        return "  HOME = '/'";
+      }
+
+      if (variableName === '404') {
+        return `  NOT_FOUND = '/${variableName}'`;
+      }
+
+      return `  ${variableName.toUpperCase()} = '/${name}'`;
+    })
+    .sort()
+    .join(',\n');
+
+const writeEnumToRoutesFile = (enumContent) => {
+  const path = getRouteTypesDirectoryPath();
+
+  fs.writeFileSync(path, enumContent);
+};
+
+const main = async () => {
+  const pagesDirectoryPath = getPagesDirectoryPath();
+
+  const filenames = await getFilenamesFromDirectory(pagesDirectoryPath);
+  const filteredFilenames = filterOutFilesWithFloorAtBeginning(filenames);
+  const filenamesWithoutExtensions = removeExtensionsFromFilenames(filteredFilenames);
+
+  const enumBody = convertNamesToEnumBody(filenamesWithoutExtensions);
+
+  const enumContent = `export enum ERoutes {\n${enumBody}\n}\n`;
+
+  writeEnumToRoutesFile(enumContent);
+};
+
+main();
---
+++ src/infrastructure/types/routes.ts
@@ -0,0 +1,8 @@
+export enum ERoutes {
+  DASHBOARD = '/dashboard',
+  HOME = '/',
+  NOT_FOUND = '/404',
+  ONBOARDING = '/onboarding',
+  PRIVACY_POLICY = '/privacy-policy',
+  TERMS = '/terms',
+}
