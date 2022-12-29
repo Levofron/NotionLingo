75a7f344bbd3d5bdb7a4ae39375a9da48dbfb6ac
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2022-12-29 04:18:11 +0100

    feat: improve user provider to get avatar url (#110)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  reactStrictMode: false,
   disable: process.env.NEXT_PUBLIC_APP_ENV === 'local',
 });
 
--- src/infrastructure/context/user/user.types.ts
+++ src/infrastructure/context/user/user.types.ts
@@ -1,8 +1,10 @@
+import { IUser } from '@domain/rest/rest.models';
 import { IOAuthResponse, TUser } from '@domain/supabase/supabase.types';
 
 export interface IUserContextValue {
+  isLoading: boolean;
   isUserAuthenticated: boolean;
   loginViaGoogle: () => Promise<IOAuthResponse>;
   logout: () => void;
-  user: TUser | null;
+  user: (TUser & IUser) | null;
 }
