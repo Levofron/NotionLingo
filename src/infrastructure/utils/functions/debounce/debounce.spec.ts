3f63624130affb63f763743e772a7117939afa55
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-02 19:57:54 +0100

    chore: updat

--- src/infrastructure/utils/functions/debounce/debounce.spec.ts
+++ src/infrastructure/utils/functions/debounce/debounce.spec.ts
@@ -18,4 +18,31 @@ describe('debounce function', () => {
 
     expect(callback).toHaveBeenCalled();
   });
+
+  it('should call the callback only once', () => {
+    const callback = jest.fn();
+    const debouncedCallback = debounce(callback, 100);
+
+    debouncedCallback();
+    debouncedCallback();
+    debouncedCallback();
+    debouncedCallback();
+    debouncedCallback();
+
+    jest.advanceTimersByTime(100);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the callback with the correct arguments', () => {
+    const callback = jest.fn();
+    const debouncedCallback = debounce(callback, 100);
+
+    debouncedCallback('foo', 'bar');
+    debouncedCallback('bar', 'foo');
+
+    jest.advanceTimersByTime(100);
+
+    expect(callback).toHaveBeenCalledWith('bar', 'foo');
+  });
 });
