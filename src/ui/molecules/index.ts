b3f4b998391742e6811ead279c06217ece4db630
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-01-03 07:32:57 +0100

    feat: create onboarding step two component (#124)

--- src/ui/molecules/index.ts
+++ src/ui/molecules/index.ts
@@ -8,3 +8,4 @@ export * from './sidebar-content/sidebar-content.component';
 export * from './getting-started/getting-started.component';
 export * from './textarea-control/textarea-control.component';
 export * from './onboarding-step-one/onboarding-step-one.component';
+export * from './onboarding-step-two/onboarding-step-two.component';
