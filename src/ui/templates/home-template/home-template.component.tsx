7d8a45aa8591a3fea1f52a963d10848739f4c3a4
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-06 12:28:08 +0100

    chore: create full screen loader

--- src/ui/templates/home-template/home-template.component.tsx
+++ src/ui/templates/home-template/home-template.component.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 
-import { Footer, FullScreenLoader, GettingStarted, HomeHero, TryItNow } from '@ui/molecules';
+import { Footer, GettingStarted, HomeHero, TryItNow } from '@ui/molecules';
 import { ContactForm } from '@ui/organisms';
 
 import { useUser } from '@infrastructure/utils';
 
 export const HomeTemplate: FC = (): JSX.Element => {
   const { user } = useUser();
-
-  return <FullScreenLoader />;
 
   return (
     <div>
