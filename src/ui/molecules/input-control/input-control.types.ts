a6a8a1b95b646892f4bbeee850fcbee3ddee4c59
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-06 18:34:49 +0100

    chore: connect third onboarding step with api

--- src/ui/molecules/input-control/input-control.types.ts
+++ src/ui/molecules/input-control/input-control.types.ts
@@ -3,6 +3,7 @@ import { IInputProps } from '@ui/atoms';
 export interface IInputControlProps extends IInputProps {
   errorMessage?: string;
   isDisabled?: boolean;
+  isLoading?: boolean;
   isRequired?: boolean;
   label?: string;
 }
