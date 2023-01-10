51143f9eb7e132af67dfe8ab7db7248ab054d7d3
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-01-10 06:51:27 +0100

    chore: lock body scroll when sidebar is open (#143)

--- src/ui/organisms/sidebar-with-header/sidebar-with-header.component.tsx
+++ src/ui/organisms/sidebar-with-header/sidebar-with-header.component.tsx
@@ -1,11 +1,19 @@
 import { useDisclosure } from '@chakra-ui/react';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import { Box } from '@ui/atoms';
 import { Header, SidebarContent } from '@ui/molecules';
 
 export const SidebarWithHeader: FC = (): JSX.Element => {
   const { isOpen, onClose, onOpen } = useDisclosure();
+
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? 'hidden' : 'scroll';
+
+    return () => {
+      document.body.style.overflow = 'scroll';
+    };
+  }, [isOpen]);
 
   return (
     <Box bg="gray.100" overflow="hidden">
