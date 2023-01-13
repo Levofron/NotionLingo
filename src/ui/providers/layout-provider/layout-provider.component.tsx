2cd89d193583b21156fa928244dc449c48f545fb
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-01-13 21:19:32 +0100

    feat: create global layout provider (#152)

---
+++ src/ui/providers/layout-provider/layout-provider.component.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+import { Box } from '@ui/atoms';
+
+import { ILayoutProviderProps } from './layout-provider.types';
+
+export const LayoutProvider: FC<ILayoutProviderProps> = ({ children }): JSX.Element => (
+  <>
+    <style global jsx>{`
+      body,
+      html,
+      #__next {
+        height: 100%;
+      }
+    `}</style>
+    <Box bg="gray.50" height="100%" overflow="scroll" width="100%">
+      {children}
+    </Box>
+  </>
+);
