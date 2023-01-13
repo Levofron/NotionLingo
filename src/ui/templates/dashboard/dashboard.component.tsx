51db5d9896093cc4d033842e7e17f5d1c52095ab
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-13 16:16:42 +0100

    feat: create notion word card

--- src/ui/templates/dashboard/dashboard.component.tsx
+++ src/ui/templates/dashboard/dashboard.component.tsx
@@ -1,4 +1,3 @@
-import { Center } from '@chakra-ui/react';
 import { FC } from 'react';
 
 import { NotionWordCard } from '@ui/molecules';
@@ -7,12 +6,10 @@ import { IDashboardProps } from './dashboard.types';
 
 export const DashboardTemplate: FC<IDashboardProps> = (): JSX.Element => (
   <div className="block">
-    <Center>
-      <NotionWordCard
-        exampleSentence="I killed two birds with one stone and picked the kids up on the way to the supermarket."
-        meaning="to succeed in achieving two things in a single action"
-        word="Kill two birds with one stone"
-      />
-    </Center>
+    <NotionWordCard
+      exampleSentence="I killed two birds with one stone and picked the kids up on the way to the supermarket."
+      meaning="to succeed in achieving two things in a single action"
+      word="Kill two birds with one stone"
+    />
   </div>
 );
