322dc8c532138e0f62fe4dd2f2ad92c77b962ca6
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-14 10:13:58 +0100

    chore: update

--- src/infrastructure/utils/hooks/use-window-size/use-window-size.spec.ts
+++ src/infrastructure/utils/hooks/use-window-size/use-window-size.spec.ts
@@ -1,3 +1,5 @@
+import { renderHook } from '@testing-library/react-hooks';
+
 import { functionImportTest } from '@infrastructure/utils';
 
 import { useWindowSize } from './use-window-size.hook';
@@ -5,5 +7,17 @@ import { useWindowSize } from './use-window-size.hook';
 describe('useWindowSize hook', () => {
   functionImportTest(useWindowSize);
 
-  it('', () => {});
+  it('should return an object with width and height properties', () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current).toHaveProperty('width');
+    expect(result.current).toHaveProperty('height');
+  });
+
+  it('should return the correct width and height', () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current.width).toEqual(window.innerWidth);
+    expect(result.current.height).toEqual(window.innerHeight);
+  });
 });
