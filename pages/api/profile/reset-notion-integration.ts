c9be9c4941174579f954140228caf2f258f212ee
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-25 21:21:59 +0100

    feat: create reset notion integration endpoint

---
+++ pages/api/profile/reset-notion-integration.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { supabaseInstance } from '@infrastructure/config';
+import { EHttpStatusCode } from '@infrastructure/types/http-status-code';
+import {
+  assignRequestTokenToSupabaseSessionMiddleware,
+  getUserFromRequest,
+  validateIfUserIsLoggedInMiddleware,
+  validateRequestMethodMiddleware,
+  validateRouteSecretMiddleware,
+  withMiddleware,
+} from '@infrastructure/utils/node';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const user = await getUserFromRequest(req);
+
+  const { data: updatedProfileData } = await supabaseInstance
+    .from('profiles')
+    .update({
+      notion_api_key: null,
+      notion_page_id: null,
+    })
+    .eq('id', user?.id)
+    .throwOnError()
+    .single();
+
+  return res.status(EHttpStatusCode.OK).json({
+    notionApiKey: updatedProfileData?.notion_api_key,
+    notionPageId: updatedProfileData?.notion_page_id,
+  });
+};
+
+const middlewareToApply = [
+  validateRequestMethodMiddleware('GET'),
+  validateRouteSecretMiddleware,
+  validateIfUserIsLoggedInMiddleware,
+  assignRequestTokenToSupabaseSessionMiddleware,
+];
+
+export default withMiddleware(handler)(middlewareToApply);
