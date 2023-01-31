28907d0aed1a6f6d9c259013b1296829567063bc
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-31 18:55:38 +0100

    feat: create use key press mapper function

--- src/infrastructure/utils/hooks/use-key-press-mapper/use-key-press-mapper.hook.ts
+++ src/infrastructure/utils/hooks/use-key-press-mapper/use-key-press-mapper.hook.ts
@@ -10,7 +10,7 @@ export const useKeyPressMapper = (
   const handleKeyPress = debounce((event: KeyboardEvent) => {
     event.preventDefault();
 
-    if (isBlocked) {
+    if (isBlocked || !keyMappers?.length) {
       return;
     }
 
