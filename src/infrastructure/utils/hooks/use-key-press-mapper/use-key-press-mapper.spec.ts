459d5fa8fb7b0d0afafa7f3b28a9d3683fc0e725
playerony <pawel.wojtasinski.1995@gmail.com>
2023-01-31 18:51:32 +0100

    feat: create use key press mapper function

--- src/infrastructure/utils/hooks/use-key-press-mapper/use-key-press-mapper.spec.ts
+++ src/infrastructure/utils/hooks/use-key-press-mapper/use-key-press-mapper.spec.ts
@@ -1,9 +1,37 @@
+import { fireEvent } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+
 import { functionImportTest } from '@infrastructure/utils';
 
 import { useKeyPressMapper } from './use-key-press-mapper.hook';
 
+jest.useFakeTimers();
+
 describe('useKeyPressMapper hook', () => {
   functionImportTest(useKeyPressMapper);
 
-  it('', () => {});
+  it('should call callback assigned to key with "keypress" event', () => {
+    const callback = jest.fn();
+
+    renderHook(() => useKeyPressMapper([[['Space'], callback]]));
+
+    fireEvent.keyPress(document, { code: 'Space' });
+
+    jest.advanceTimersByTime(0);
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(300);
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it('should not call unassigned callback', () => {
+    const callback = jest.fn();
+
+    renderHook(() => useKeyPressMapper([[['Space'], callback]]));
+
+    fireEvent.keyPress(document, { code: 'KeyP' });
+
+    jest.advanceTimersByTime(300);
+    expect(callback).not.toHaveBeenCalled();
+  });
 });
