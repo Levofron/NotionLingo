afa7ea2f96e6457a5a2c9819ee6e623183e3f0b4
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-02-03 17:16:41 +0100

    feat: create not found page (#195)

--- src/ui/templates/index.ts
+++ src/ui/templates/index.ts
@@ -1,5 +1,6 @@
 export * from './home/home.component';
 export * from './donate/donate.component';
 export * from './dashboard/dashboard.component';
+export * from './not-found/not-found.component';
 export * from './onboarding/onboarding.component';
 export * from './account-settings/account-settings.component';
