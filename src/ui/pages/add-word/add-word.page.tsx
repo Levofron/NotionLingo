1ad2f09a60cbcca7a63c6b456e709645334e5914
playerony <pawel.wojtasinski.1995@gmail.com>
2023-02-16 19:42:00 +0100

    feat: create mvp version of add word page

--- src/ui/pages/add-word/add-word.page.tsx
+++ src/ui/pages/add-word/add-word.page.tsx
@@ -1,9 +1,11 @@
 import { SEO } from '@ui/atoms';
+import { SidebarWithHeader } from '@ui/organisms';
 import { AddWordTemplate } from '@ui/templates';
 
 export const AddWordPage = (): JSX.Element => (
   <>
     <SEO noFollow noIndex title="Add word" />
+    <SidebarWithHeader />
     <AddWordTemplate />
   </>
 );
