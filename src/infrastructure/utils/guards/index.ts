b556c8865f638f89f846fb46814ee0bed524e3d4
playerony <pawel.wojtasinski.1995@gmail.com>
2023-02-22 19:46:20 +0100

    feat: create use copy to clipboard hook

--- src/infrastructure/utils/guards/index.ts
+++ src/infrastructure/utils/guards/index.ts
@@ -2,7 +2,6 @@ export * from './is-error/is-error.function';
 export * from './is-string/is-string.function';
 export * from './is-object/is-object.function';
 export * from './is-boolean/is-boolean.function';
-export * from './is-promise/is-promise.function';
 export * from './object-keys/object-keys.function';
 export * from './is-function/is-function.function';
 export * from './has-own-property/has-own-property.function';
--- src/infrastructure/utils/guards/is-promise/is-promise.function.ts
+++
@@ -1,5 +0,0 @@
-import { isFunction, isObject } from '@infrastructure/utils';
-
-export const isPromise = (functionToCheck: unknown): functionToCheck is Promise<unknown> =>
-  // @ts-expect-error
-  (isFunction(functionToCheck) || isObject(functionToCheck)) && isFunction(functionToCheck.then);
