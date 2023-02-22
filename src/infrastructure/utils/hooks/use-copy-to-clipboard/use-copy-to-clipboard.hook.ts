a720bb3e8e8d7c8f5f90054f91fb5ba1ba62f87b
playerony <pawel.wojtasinski.1995@gmail.com>
2023-02-22 19:48:43 +0100

    feat: create use copy to clipboard hook

--- src/infrastructure/utils/hooks/use-copy-to-clipboard/use-copy-to-clipboard.hook.ts
+++ src/infrastructure/utils/hooks/use-copy-to-clipboard/use-copy-to-clipboard.hook.ts
@@ -3,28 +3,16 @@ import { useState } from 'react';
 export const useCopyToClipboard = () => {
   const [copiedText, setCopiedText] = useState<string | null>(null);
 
-  const copyTextToClipboard = async (text: string) => {
-    if (!navigator?.clipboard) {
-      console.warn('Clipboard not supported');
-
-      return false;
-    }
-
-    await navigator.clipboard.writeText(text);
-    setCopiedText(text);
-
-    return true;
-  };
-
   const copyToClipboard = async (text: string) => {
     if (!navigator?.clipboard) {
-      console.warn('Clipboard not supported');
-
       return false;
     }
 
     try {
-      return await copyTextToClipboard(text);
+      await navigator.clipboard.writeText(text);
+      setCopiedText(text);
+
+      return true;
     } catch {
       setCopiedText(null);
 
