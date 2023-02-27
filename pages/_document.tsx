6bab8d4d83fa98f5ca9b945dbf09cbb266bddd55
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-02-27 01:26:40 +0100

    fix: add head tag in doc to fix build (#258)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,10 @@
-import Document, { Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
