ada87b36466da25fb8bd5b702288eb5a9a1709dc
playerony <pawel.wojtasinski.1995@gmail.com>
2023-02-28 19:38:21 +0100

    fix: input should not jump on error

--- src/ui/molecules/input-control/input-control.component.tsx
+++ src/ui/molecules/input-control/input-control.component.tsx
@@ -1,3 +1,4 @@
+import { FormHelperText } from '@chakra-ui/react';
 import { FC } from 'react';
 
 import { FormControl, FormErrorMessage, FormLabel, Input, Spinner } from '@ui/atoms';
@@ -12,15 +13,25 @@ export const InputControl: FC<IInputControlProps> = ({
   label,
   mode,
   ...inputProps
-}): JSX.Element => (
-  <FormControl isDisabled={isDisabled} isInvalid={!!errorMessage} isRequired={isRequired}>
-    {label ? (
-      <FormLabel color={mode === 'light' ? 'gray.50' : 'gray.900'}>
-        {label}
-        {isLoading ? <Spinner ml="5px" size="xs" /> : null}
-      </FormLabel>
-    ) : null}
-    <Input mode={mode} {...inputProps} />
-    {errorMessage ? <FormErrorMessage>{errorMessage}</FormErrorMessage> : null}
-  </FormControl>
-);
+}): JSX.Element => {
+  console.log(errorMessage);
+
+  return (
+    <FormControl isDisabled={isDisabled} isInvalid={!!errorMessage} isRequired={isRequired}>
+      {label ? (
+        <FormLabel color={mode === 'light' ? 'gray.50' : 'gray.900'}>
+          {label}
+          {isLoading ? <Spinner ml="5px" size="xs" /> : null}
+        </FormLabel>
+      ) : null}
+      <Input mode={mode} {...inputProps} />
+      {errorMessage ? (
+        <FormErrorMessage height={4} lineHeight="normal">
+          {errorMessage}
+        </FormErrorMessage>
+      ) : (
+        <FormHelperText height={4} />
+      )}
+    </FormControl>
+  );
+};
