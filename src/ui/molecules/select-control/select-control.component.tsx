92614f1367712a66e2d82e189a95a6b89a146e05
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-01 20:20:49 +0100

    fix: select should not jumo on error

--- src/ui/molecules/select-control/select-control.component.tsx
+++ src/ui/molecules/select-control/select-control.component.tsx
@@ -1,6 +1,13 @@
 import { FC } from 'react';
 
-import { FormControl, FormErrorMessage, FormLabel, Select, Spinner } from '@ui/atoms';
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Select,
+  Spinner,
+} from '@ui/atoms';
 
 import { ISelectControlProps } from './select-control.types';
 
@@ -14,12 +21,18 @@ export const SelectControl: FC<ISelectControlProps> = ({
 }): JSX.Element => (
   <FormControl isDisabled={isDisabled} isInvalid={!!errorMessage} isRequired={isRequired}>
     {label ? (
-      <FormLabel color="gray.900">
+      <FormLabel color="gray.900" mb="3px">
         {label}
         {isLoading ? <Spinner ml="5px" size="xs" /> : null}
       </FormLabel>
     ) : null}
     <Select {...selectProps} />
-    {errorMessage ? <FormErrorMessage>{errorMessage}</FormErrorMessage> : null}
+    {errorMessage ? (
+      <FormErrorMessage height={4} lineHeight="normal" mt="3px">
+        {errorMessage}
+      </FormErrorMessage>
+    ) : (
+      <FormHelperText height={4} mt="3px" />
+    )}
   </FormControl>
 );
