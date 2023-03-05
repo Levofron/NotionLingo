77aa481de9d4158a4649f50f07f2c56cfb8968e9
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-03-05 16:08:01 +0100

    feat: extract motion icon button to molecule (#290)

--- client/ui/molecules/index.ts
+++ client/ui/molecules/index.ts
@@ -14,7 +14,7 @@ export * from './getting-started/getting-started.component';
 export * from './textarea-control/textarea-control.component';
 export * from './full-screen-loader/full-screen-loader.component';
 export * from './confirmation-modal/confirmation-modal.component';
-export * from './back-to-top-button/back-to-top-button.component';
+export * from './motion-icon-button/motion-icon-button.component';
 export * from './onboarding-tab-list/onboarding-tab-list.component';
 export * from './onboarding-step-layout/onboarding-step-layout.component';
 export * from './available-notion-database/available-notion-database.component';
