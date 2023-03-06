87fdee8c1ef182871404d8c3810fc7fba9834147
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-06 22:01:43 +0100

    fix: extract execute command function and fix an error

--- pages/api/profile/increase-daily-streak.ts
+++ pages/api/profile/increase-daily-streak.ts
@@ -8,6 +8,7 @@ import {
   assignRequestTokenToSupabaseSessionMiddleware,
   getProfileById,
   getUserFromRequest,
+  isIsoDate,
   validateIfParametersExistsMiddleware,
   validateIfUserIsLoggedInMiddleware,
   validateRequestMethodMiddleware,
@@ -16,16 +17,6 @@ import {
 } from '@server/utils';
 
 import { supabaseInstance } from '@config/supabase/supabase.instance';
-
-const isIsoDate = (string: string) => {
-  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(string)) {
-    return false;
-  }
-
-  const date = new Date(string);
-
-  return date instanceof Date && !Number.isNaN(date) && date.toISOString() === string;
-};
 
 const updateProfileDetailsDecorator =
   (userId: string, currentDate: Date, res: NextApiResponse) => async (data: object) => {
