b8e1194a27c62d3c8d60ecf0ac116a752569a63c
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-12 13:50:35 +0100

    feat: add lang parameter to html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         <body>
           <Main />
