ee004d3a3b918e2488a539f4461a31c0c881d8ff
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-12 22:11:09 +0100

    chore: use mjs extension for custom node scripts

--- scripts/node/generate-robots-txt.mjs
+++ scripts/node/generate-robots-txt.mjs
@@ -7,14 +7,12 @@ const getPath = (destination) => {
   return path.join(currentDirectory, destination);
 };
 
-// eslint-disable-next-line operator-linebreak
 const crawlableRobotsTxt =
   'User-agent: *\nAllow: /\n\nSitemap: https://www.notionlingo.com/sitemap.xml';
 
 const uncrawlableRobotsTxt = 'User-agent: *\nDisallow: /';
 
 function generateRobotsTxt() {
-  // eslint-disable-next-line operator-linebreak
   const robotsTxt =
     process.env.VERCEL_ENV === 'production' ? crawlableRobotsTxt : uncrawlableRobotsTxt;
 
