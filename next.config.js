e9bb741f900e9db189c8484a10b4d2de5ecf46a1
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-17 13:14:45 +0100

    feat: configure build analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const nextPwa = require('next-pwa');
 const { withAxiom } = require('next-axiom');
+const nextBundleAnalyzer = require('@next/bundle-analyzer');
 
 const withPwa = nextPwa({
   dest: 'public',
@@ -8,18 +9,24 @@ const withPwa = nextPwa({
   disable: ['local', 'test'].includes(process.env.NEXT_PUBLIC_APP_ENV),
 });
 
-module.exports = withPwa(
-  withAxiom({
-    swcMinify: true,
-    reactStrictMode: false,
-    images: {
-      domains: [
-        'images.pexels.com',
-        'source.unsplash.com',
-        'www.buymeacoffee.com',
-        'www.paypalobjects.com',
-        'lh3.googleusercontent.com',
-      ],
-    },
-  }),
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(
+  withPwa(
+    withAxiom({
+      swcMinify: true,
+      reactStrictMode: false,
+      images: {
+        domains: [
+          'images.pexels.com',
+          'source.unsplash.com',
+          'www.buymeacoffee.com',
+          'www.paypalobjects.com',
+          'lh3.googleusercontent.com',
+        ],
+      },
+    }),
+  ),
 );
