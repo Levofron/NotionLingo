76960fc6b2a9a45f27c6c0bed9435f139f31fb5c
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-17 12:33:34 +0100

    feat: scroll to top on route change

--- client/infrastructure/utils/functions/index.ts
+++ client/infrastructure/utils/functions/index.ts
@@ -1,3 +1,4 @@
 export * from './debounce/debounce.function';
+export * from './scroll-to-top/scroll-to-top.function';
 export * from './clean-up-string/clean-up-string.function';
 export * from './capitalize-first-letter/capitalize-first-letter.function';
