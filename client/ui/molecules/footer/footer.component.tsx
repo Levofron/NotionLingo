f8180c9339598d710a7681421c4d276bff1653eb
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-03-17 13:36:03 +0100

    feat: simplify footer design (#356)

--- client/ui/molecules/footer/footer.component.tsx
+++ client/ui/molecules/footer/footer.component.tsx
@@ -1,87 +1,15 @@
-import NextImage from 'next/image';
 import { FC } from 'react';
 
-import { Box, ChakraNextLink, Container, SimpleGrid, Stack, Text } from '@ui/atoms';
-
-import { ERoutes } from '@infrastructure/types/routes';
-
-import {
-  BUY_ME_A_COFFEE_LINK,
-  GITHUB_LINK,
-  LINKEDIN_LINK,
-  PAYPAL_LINK,
-  TWITTER_LINK,
-} from '@config/constants';
+import { Box, Text } from '@ui/atoms';
 
 export const Footer: FC = (): JSX.Element => {
   const currentYear = new Date().getUTCFullYear();
 
   return (
-    <Box bg="gray.900">
-      <Container maxW="6xl" py={10}>
-        <SimpleGrid columns={{ base: 1, sm: 3 }} spacing={8}>
-          <Stack align="flex-start">
-            <Text color="gray.50" fontSize="lg" fontWeight="600" mb={2}>
-              Legal
-            </Text>
-            <ChakraNextLink color="gray.50" href={ERoutes.PRIVACY_POLICY}>
-              Privacy Policy
-            </ChakraNextLink>
-            <ChakraNextLink color="gray.50" href={ERoutes.TERMS}>
-              Terms & Conditions
-            </ChakraNextLink>
-          </Stack>
-          <Stack align="flex-start">
-            <Text color="gray.50" fontSize="lg" fontWeight="600" mb={2}>
-              Follow us
-            </Text>
-            <ChakraNextLink color="gray.50" href={GITHUB_LINK} target="_blank">
-              GitHub
-            </ChakraNextLink>
-            <ChakraNextLink color="gray.50" href={TWITTER_LINK} target="_blank">
-              Twitter
-            </ChakraNextLink>
-            <ChakraNextLink color="gray.50" href={LINKEDIN_LINK} target="_blank">
-              LinkedIn
-            </ChakraNextLink>
-          </Stack>
-          <Stack align="flex-start">
-            <Text color="gray.50" fontSize="lg" fontWeight="600" mb={2}>
-              Support us
-            </Text>
-            <ChakraNextLink href={BUY_ME_A_COFFEE_LINK} target="_blank">
-              <NextImage
-                alt="Buy Me A Coffee"
-                height={40}
-                src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-1.svg"
-                width={150}
-              />
-            </ChakraNextLink>
-            <ChakraNextLink
-              href={PAYPAL_LINK}
-              style={{
-                backgroundColor: 'white',
-                padding: '3px 10px',
-                borderRadius: '6px',
-                width: '150px',
-              }}
-              target="_blank"
-            >
-              <NextImage
-                alt="PayPal"
-                height={40}
-                src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-color.svg"
-                width={146}
-              />
-            </ChakraNextLink>
-          </Stack>
-        </SimpleGrid>
-      </Container>
-      <Box py={{ base: 6, sm: 10 }}>
-        <Text color="gray.50" pt={{ base: 2, sm: 6 }} textAlign="center">
-          © {currentYear} Paweł Wojtasiński. All rights reserved.
-        </Text>
-      </Box>
+    <Box bg="gray.900" py={{ base: 12, md: 20 }}>
+      <Text color="gray.50" textAlign="center">
+        © {currentYear} Paweł Wojtasiński. All rights reserved.
+      </Text>
     </Box>
   );
 };
