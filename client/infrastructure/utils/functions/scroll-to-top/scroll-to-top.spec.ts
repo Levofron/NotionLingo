60a25cb5ab380a8c06e0478bebbcc6523b0606ba
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-20 19:28:06 +0100

    test: add missed tests for scroll to top function

--- client/infrastructure/utils/functions/scroll-to-top/scroll-to-top.spec.ts
+++ client/infrastructure/utils/functions/scroll-to-top/scroll-to-top.spec.ts
@@ -2,7 +2,38 @@ import { functionImportTest } from '@infrastructure/utils';
 
 import { scrollToTop } from './scroll-to-top.function';
 
-// TODO - Add tests
 describe('scrollToTop function', () => {
   functionImportTest(scrollToTop);
+
+  it('should rerutn undefined if element is null', () => {
+    expect(scrollToTop(null)).toBeUndefined();
+  });
+
+  it('should call scrollTo method on element', () => {
+    const element = {
+      scrollTo: jest.fn(),
+    };
+
+    // @ts-expect-error
+    scrollToTop(element);
+
+    expect(element.scrollTo).toHaveBeenCalledWith({
+      top: 0,
+      behavior: 'auto',
+    });
+  });
+
+  it('should call scrollTo method on element with smooth behavior', () => {
+    const element = {
+      scrollTo: jest.fn(),
+    };
+
+    // @ts-expect-error
+    scrollToTop(element, 'smooth');
+
+    expect(element.scrollTo).toHaveBeenCalledWith({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
 });
