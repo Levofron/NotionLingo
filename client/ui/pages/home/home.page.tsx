47109a9a450752863449440c5f7dd3d05bb9801f
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-04-03 18:07:22 +0200

    refactor: follow atomic design for home page (#384)

--- client/ui/pages/home/home.page.tsx
+++ client/ui/pages/home/home.page.tsx
@@ -1,35 +1,3 @@
-import { FAQPageJsonLd, LogoJsonLd, SocialProfileJsonLd } from 'next-seo';
+import { Home as HomeTemplate } from '@ui/templates';
 
-import { SEO } from '@ui/atoms';
-import { SidebarWithHeader } from '@ui/organisms';
-import { HomeTemplate } from '@ui/templates';
-
-import {
-  BUY_ME_A_COFFEE_LINK,
-  GITHUB_LINK,
-  LINKEDIN_LINK,
-  PAYPAL_LINK,
-  QUESTIONS_AND_ANSWERS,
-  TWITTER_LINK,
-} from '@config/constants';
-
-export const HomePage = (): JSX.Element => (
-  <>
-    <SEO title="Improve vocabulary with your Notion database" />
-    <LogoJsonLd logo="https://notionlingo.com/apple-touch-icon.png" url="https://notionlingo.com" />
-    <SocialProfileJsonLd
-      name="Paweł Wojtasiński"
-      sameAs={[LINKEDIN_LINK, GITHUB_LINK, TWITTER_LINK, BUY_ME_A_COFFEE_LINK, PAYPAL_LINK]}
-      type="Person"
-      url={LINKEDIN_LINK}
-    />
-    <FAQPageJsonLd
-      mainEntity={QUESTIONS_AND_ANSWERS.map((_questionAndAnswer) => ({
-        questionName: _questionAndAnswer.question,
-        acceptedAnswerText: _questionAndAnswer.answer,
-      }))}
-    />
-    <SidebarWithHeader />
-    <HomeTemplate />
-  </>
-);
+export const Home = () => <HomeTemplate />;
