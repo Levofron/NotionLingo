4e1b42769eea487f8a0888ad1726b2edcb382189
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-04-08 19:35:51 +0200

    chore: represent testimonials as array (#399)

--- client/ui/molecules/testimonials/testimonials.component.tsx
+++ client/ui/molecules/testimonials/testimonials.component.tsx
@@ -2,9 +2,9 @@ import { FC } from 'react';
 
 import { Avatar, Box, Container, Flex, Heading, Stack, Text } from '@ui/atoms';
 
-import { ITestimonialsProps } from './testimonials.types';
+import { TESTIMONIALS } from './testimonials.defaults';
 
-export const Testimonials: FC<ITestimonialsProps> = (): JSX.Element => (
+export const Testimonials: FC = (): JSX.Element => (
   <Box bg="gray.100">
     <Container gap={12} maxW="6xl" py={{ base: 14, sm: 20, md: 40 }}>
       <Stack align="center" mb={{ base: 14, sm: 20 }} spacing={0}>
@@ -18,56 +18,50 @@ export const Testimonials: FC<ITestimonialsProps> = (): JSX.Element => (
         gap={{ base: 10, md: 4, lg: 10 }}
         mt={{ base: 8, md: 10 }}
       >
-        {Array.from({ length: 3 })
-          .fill(0)
-          .map((_, i) => (
-            <Box key={i}>
-              <Flex
-                _after={{
-                  content: '""',
-                  w: 0,
-                  h: 0,
-                  borderLeft: 'solid transparent',
-                  borderLeftWidth: 16,
-                  borderRight: 'solid transparent',
-                  borderRightWidth: 16,
-                  borderTop: 'solid',
-                  borderTopWidth: 16,
-                  borderTopColor: 'white',
-                  pos: 'absolute',
-                  bottom: '-16px',
-                  left: '50%',
-                  transform: 'translateX(-50%)',
-                }}
-                align="center"
-                bg="white"
-                boxShadow="lg"
-                flexDirection="column"
-                gap={{ base: 2, sm: 3 }}
-                p={8}
-                position="relative"
-                rounded="xl"
-              >
-                <Heading>Efficient Collaborating</Heading>
-                <Text color="gray.600" fontSize="sm" textAlign="center">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed
-                  imperdiet nibh lectus feugiat nunc sem.
+        {TESTIMONIALS.map((_testimonial) => (
+          <Box key={_testimonial.heading}>
+            <Flex
+              _after={{
+                content: '""',
+                w: 0,
+                h: 0,
+                borderLeft: 'solid transparent',
+                borderLeftWidth: 16,
+                borderRight: 'solid transparent',
+                borderRightWidth: 16,
+                borderTop: 'solid',
+                borderTopWidth: 16,
+                borderTopColor: 'white',
+                pos: 'absolute',
+                bottom: '-16px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+              }}
+              align="center"
+              bg="white"
+              boxShadow="lg"
+              flexDirection="column"
+              gap={{ base: 2, sm: 3 }}
+              p={8}
+              position="relative"
+              rounded="xl"
+            >
+              <Heading>{_testimonial.heading}</Heading>
+              <Text color="gray.600" fontSize="sm" textAlign="center">
+                {_testimonial.description}
+              </Text>
+            </Flex>
+            <Flex align="center" direction="column" mt={8}>
+              <Avatar mb={2} src={_testimonial.imageSrc} />
+              <Stack align="center" spacing={-1}>
+                <Text fontWeight={600}>{_testimonial.name}</Text>
+                <Text color="gray.600" fontSize="sm">
+                  {_testimonial.position}
                 </Text>
-              </Flex>
-              <Flex align="center" direction="column" mt={8}>
-                <Avatar
-                  mb={2}
-                  src="https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-                />
-                <Stack align="center" spacing={-1}>
-                  <Text fontWeight={600}>Jane Cooper</Text>
-                  <Text color="gray.600" fontSize="sm">
-                    CEO at ABC Corporation
-                  </Text>
-                </Stack>
-              </Flex>
-            </Box>
-          ))}
+              </Stack>
+            </Flex>
+          </Box>
+        ))}
       </Flex>
     </Container>
   </Box>
---
+++ client/ui/molecules/testimonials/testimonials.defaults.ts
@@ -0,0 +1,31 @@
+import { ITestimonial } from './testimonials.types';
+
+export const TESTIMONIALS: ITestimonial[] = [
+  {
+    heading: 'Efficient Collaborating',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.',
+    imageSrc:
+      'https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    name: 'John Doe',
+    position: 'CEO, Company',
+  },
+  {
+    heading: 'Efficient Collaborating',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.',
+    imageSrc:
+      'https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    name: 'John Doe',
+    position: 'CEO, Company',
+  },
+  {
+    heading: 'Efficient Collaborating',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat nunc sem.',
+    imageSrc:
+      'https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    name: 'John Doe',
+    position: 'CEO, Company',
+  },
+];
