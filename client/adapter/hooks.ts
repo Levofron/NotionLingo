c3ac2aa460ba454141ffb18e518b146f5e4babe4
playerony <pawel.wojtasinski.1995@gmail.com>
2023-04-09 15:30:41 +0200

    ci: initial configuration of depcruise

--- client/adapter/hooks.ts
+++ client/adapter/hooks.ts
@@ -1,10 +1,6 @@
 import { restModule } from '@adapter/modules';
 
-import { findNotionWordById } from '@domain/rest/operations';
-
 import { useAxios } from '@infrastructure/hooks';
-
-console.log(findNotionWordById);
 
 export const useUpdateWord = () => {
   const { isLoading, mutateAsync } = useAxios(restModule.updateNotionWord);
