36c54f1e40a61e639a981ff76ad4a2512dcf908d
playerony <pawel.wojtasinski.1995@gmail.com>
2023-04-11 19:16:56 +0200

    chore(deps): bump

--- client/presentation/atoms/list-icon/list-icon.types.ts
+++ client/presentation/atoms/list-icon/list-icon.types.ts
@@ -1,6 +1,5 @@
 import { As, IconProps } from '@chakra-ui/react';
 
 export interface IListIconProps extends IconProps {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  as?: As<any>;
+  as?: As;
 }
