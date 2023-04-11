5a4e2705af4061c73eab8aba178f375941217286
playerony <pawel.wojtasinski.1995@gmail.com>
2023-04-11 18:55:33 +0200

    refactor: remove files with empty tests

--- client/infrastructure/hooks/use-copy-to-clipboard/use-copy-to-clipboard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@infrastructure/functions';
-
-import { useCopyToClipboard } from './use-copy-to-clipboard.hook';
-
-describe('useCopyToClipboard hook', () => {
-  functionImportTest(useCopyToClipboard);
-});
--- client/repositories/speech-synthesis/operations/get-speech-synthesis-value-from-local-storage/get-speech-synthesis-value-from-local-storage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@infrastructure/functions';
-
-import { getSpeechSynthesisValueFromLocalStorage } from './get-speech-synthesis-value-from-local-storage.function';
-
-describe('getSpeechSynthesisValueFromLocalStorage function', () => {
-  functionImportTest(getSpeechSynthesisValueFromLocalStorage);
-});
