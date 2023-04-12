384c67c00ba86d4edf576f53c1e72ea5d07d569f
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-04-12 06:38:45 +0200

    refactor: remove files with empty tests (#417)

--- client/presentation/app/utils/fill-up-missed-local-storage-fields/fill-up-missed-local-storage-fields.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@infrastructure/functions';
-
-import { fillUpMissedLocalStorageFields } from './fill-up-missed-local-storage-fields.function';
-
-describe('fillUpMissedLocalStorageFields function', () => {
-  functionImportTest(fillUpMissedLocalStorageFields);
-});
--- client/repositories/speech-synthesis/operations/set-speech-synthesis-value-to-local-storage/set-speech-synthesis-value-to-local-storage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { functionImportTest } from '@infrastructure/functions';
-
-import { setSpeechSynthesisValueToLocalStorage } from './set-speech-synthesis-value-to-local-storage.function';
-
-describe('setSpeechSynthesisValueToLocalStorage function', () => {
-  functionImportTest(setSpeechSynthesisValueToLocalStorage);
-});
