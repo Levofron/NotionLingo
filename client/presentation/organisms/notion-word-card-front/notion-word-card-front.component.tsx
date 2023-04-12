0bb820e7e3125a903c67d8d2b3538fa3d2dc194d
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-04-12 17:35:24 +0200

    fix: border radius on suggestion (#404)

--- client/presentation/organisms/notion-word-card-front/notion-word-card-front.component.tsx
+++ client/presentation/organisms/notion-word-card-front/notion-word-card-front.component.tsx
@@ -60,7 +60,7 @@ export const NotionWordCardFront: FC<INotionWordCardFrontProps> = ({
   const hasExampleSentenceSuggestion = hasSuggestion(exampleSentence, exampleSentenceSuggestion);
 
   return (
-    <Card width={{ base: 300, sm: 350, md: 400 }}>
+    <Card overflow="hidden" width={{ base: 300, sm: 350, md: 400 }}>
       {hasMeaningSuggestion || hasExampleSentenceSuggestion ? (
         <Button
           display={isTopCard ? 'flex' : 'none'}
@@ -124,6 +124,7 @@ export const NotionWordCardFront: FC<INotionWordCardFrontProps> = ({
           <Text
             border="1px solid"
             borderColor="gray.900"
+            borderRadius={10}
             color="gray.900"
             cursor={cursor}
             fontSize={{ base: 'sm', sm: 'md' }}
@@ -157,6 +158,7 @@ export const NotionWordCardFront: FC<INotionWordCardFrontProps> = ({
               <Text
                 border="1px solid"
                 borderColor="gray.900"
+                borderRadius={10}
                 color="gray.900"
                 cursor={cursor}
                 fontSize={{ base: 'sm', sm: 'md' }}
