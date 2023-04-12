f6a6b4739b8b40e07c464c0b8c649b61b8df6711
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-04-12 06:38:23 +0200

    chore(deps): bump (#418)

--- client/presentation/atoms/icon/icon.types.ts
+++ client/presentation/atoms/icon/icon.types.ts
@@ -1,6 +1,5 @@
 import { As, IconProps } from '@chakra-ui/react';
 
 export interface IIconProps extends IconProps {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  as?: As<any>;
+  as?: As;
 }
